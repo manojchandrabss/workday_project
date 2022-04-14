f398f97e9ed82beec4664e077a2642ea727a9b17
manojchandrabss <manojchandrabss@yahoo.com>
2022-04-14 14:40:01 +0800

    Update variables.tf

--- fake_terraform/variables.tf
+++ fake_terraform/variables.tf
@@ -3,3 +3,19 @@ variable "iam_users" {
   type        = list(string)
   default     = ["emma", "liam"]
 }
+
+variable "vpc-names" {
+
+  description = "creating 2 vpc's"
+  type        = list(string)
+  default     = ["fake-product-production", "Fake-product-staging"]
+
+}
+
+variable "vpc-cidr" {
+
+  description = "cidr range for 2 vpc's"
+  type        = list(any)
+  default     = ["10.0.0.0/16", "192.168.0.0/16"]
+
+}
